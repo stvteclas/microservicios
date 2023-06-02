e5072031044643f8731035df4de6067b0b92b85e
stvteclas <stvpablo@gmail.com>
2023-06-02 11:30:26 -0300

    first commit

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,35 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+    private static final Logger LOG= LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+    private final StreamRunner streamRunner;
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData,
+                                            StreamRunner runner){
+        this.twitterToKafkaServiceConfigData= configData;
+        this.streamRunner = runner;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("Apps Starts....");
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+        LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+        streamRunner.start();
+    }
+}
